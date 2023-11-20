89f45d1320e08590f2f491bf35eb785b013e8d7b
Denilson Salas G <denilsonsalguz@gmail.com>
2023-11-20 10:42:29 -0500

    saving invoices into storage class

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,12 +1,13 @@
 package storage;
 
 import java.util.ArrayList;
+
 import product.*;
+import cashRegister.Invoice;
 
 public class Storage {
-    ArrayList<Product> storageProducts = new ArrayList<Product>();
-    ArrayList<String> storagePurchasing = new ArrayList<String>();
-    ArrayList<String> storageSelling = new ArrayList<String>();
+    private ArrayList<Product> storageProducts = new ArrayList<Product>();
+    private ArrayList<Invoice> storageInvoice = new ArrayList<Invoice>();
 
     public Storage(){}
     public void SaveProduct(Product item){
@@ -20,5 +21,35 @@ public class Storage {
             return storageProducts.get(index-1);
         }
         return null;
+    }
+    public void SaveInvoice(Invoice item){
+        if(item != null){
+            storageInvoice.add(item);
+        }
+    }
+    public Invoice FindInvoice(int index){
+        int storageSize = storageInvoice.size();
+        if(index > 0 && index <= storageSize){
+            return storageInvoice.get(index-1);
+        }
+        return null;
+    }
+    public ArrayList GetAllPurchaseInovoice(){
+        ArrayList<Invoice> purchaseInvoice =new  ArrayList<Invoice>();
+        for (Invoice item:storageInvoice) {
+            if(item.getTypeInvoice().equals("Compra")){
+                purchaseInvoice.add(item);
+            }
+        }
+        return purchaseInvoice;
+    }
+    public ArrayList GetAllSellInovoice(){
+        ArrayList<Invoice> sellInvoice =new  ArrayList<Invoice>();
+        for (Invoice item:storageInvoice) {
+            if(item.getTypeInvoice().equals("Venta")){
+                sellInvoice.add(item);
+            }
+        }
+        return sellInvoice;
     }
 }
